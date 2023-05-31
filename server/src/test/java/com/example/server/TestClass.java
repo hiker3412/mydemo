8efb598aa79bcfcc4938b07c341523b5fa722081
hiker <hiker3412@gmail.com>
2023-05-31 22:19:04 +0800

    feign test

--- server/src/test/java/com/example/server/TestClass.java
+++ server/src/test/java/com/example/server/TestClass.java
@@ -4,12 +4,56 @@ import com.example.common.model.abstracttest.vo.User1;
 import com.example.common.model.abstracttest.vo.User2;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TestClass {
+    @Test
+    void netDownload() throws Exception{
+        URL url = new URL("http://news.windin.com/bulletin/79268996.pdf?mediatype=03&&pkid=79268996&&id=115467298");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        InputStream inputStream = connection.getInputStream();
+        byte[] bytes = new byte[20480];
+        System.out.println(inputStream.available());
+        int read = inputStream.read(bytes);
+        System.out.println(read);
+        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Administrator\\IdeaProjects\\mydemo\\server\\src\\test\\resources\\test.pdf");
+        fileOutputStream.write(bytes,0,read);
+    }
+
+    @Test
+    void netDownload2() throws Exception {
+        URL url = new URL("http://news.windin.com/bulletin/79268996.pdf?mediatype=03&&pkid=79268996&&id=115467298");
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        // 得到输入流
+        InputStream inputStream = conn.getInputStream();
+        // 获取自己数组
+        byte[] bytes = readInputStream(inputStream);
+        System.out.println(bytes.length);
+        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Administrator\\IdeaProjects\\mydemo\\server\\src\\test\\resources\\test2.pdf");
+        fileOutputStream.write(bytes);
+    }
+
+    public static  byte[] readInputStream(InputStream inputStream) throws IOException {
+        byte[] buffer = new byte[10240];
+        int len = 0;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        while((len = inputStream.read(buffer)) != -1) {
+            bos.write(buffer, 0, len);
+        }
+        bos.close();
+        return bos.toByteArray();
+    }
+
 
     @Test
     void printList() {
